8d70051ec9f995e444d9ee801de079801dbe9b20
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-12-12 12:42:32 +0100

    11_fill

---
+++ 11_fill.js
@@ -0,0 +1,6 @@
+//.fill() metoden ändrar element i ett array till ett visst värde
+//Ex: fill(4, 2, 5) ändrar element från postiion 2-5 till värdet 4
+
+let fruits = ['Apple', 'Orange', 'Mango', 'Banana', 'Pear']
+
+console.log(fruits.fill('Pineapple', 1, 4)) //ändrar element med positionen 1 - 4 till pineapple
